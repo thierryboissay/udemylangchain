33ed6fe79dbae85fb0d7958fdcd8b7330d660805
thierryboissay <thierry.boissay@shoreo-group.com>
2025-11-10 17:40:08 +0100

    structured_rsponse

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,9 +12,22 @@ from langchain_core.messages import HumanMessage
 from langchain_tavily import TavilySearch
 
 
+# object to describe the source tha tthe agent use to ge the answer
+#l'objet hérite de la classe BaseModel
+class Source(BaseModel):
+    """Schema for the source used by the agent"""
+    url:str = Field(description="The url of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the response returned by the agent"""
+
+    answer:str = Field(description="The answer to the question")
+    sources:List[Source] = Field(default_factory=list, description="The list of sources used to answer the question")
+
+
 llm = ChatOpenAI(model="gpt-5")
 tools=[TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
